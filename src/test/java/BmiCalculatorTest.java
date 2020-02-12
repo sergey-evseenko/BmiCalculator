9a0ad63ad572e547be3794caba5032bb4c8b0afb
sergey-evseenko <evseenkosv@tut.by>
2020-02-13 01:55:53 +0300

    Initial commit

---
+++ src/test/java/BmiCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class BmiCalculatorTest {
+
+
+}
